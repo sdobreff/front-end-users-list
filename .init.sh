b07f74b294e12eace47a1cd730621678322f1317
sdobreff <sdobreff@gmail.com>
2022-11-23 05:48:11 +0000

    vscode settings

--- .init.sh
+++ .init.sh
@@ -4,3 +4,6 @@ export REPO_NAME=$(basename $GITPOD_REPO_ROOT)
 
 sudo ln -s $GITPOD_REPO_ROOT /var/www/html/wp-content/plugins
 sudo chown gitpod:gitpod /var/www/html/wp-content/plugins/$REPO_NAME
+
+sudo ln -s $GITPOD_REPO_ROOT/gitpod-vscode /var/www/html/.vscode
+sudo chown gitpod:gitpod /var/www/html/.vscode
